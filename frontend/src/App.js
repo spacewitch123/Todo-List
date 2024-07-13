9897d6503307b022d716e847b94b04cca2f30f41
OmarRpt <93245258+OmarRpt@users.noreply.github.com>
2024-07-14 01:16:25 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,50 @@
 import { useEffect, useState } from "react";
 import AddTodo from "./components/AddTodo";
 import Todos from "./components/Todos";
-
-const axios = require("axios").default
+import api from "./api"; // Importing the API module
 
 const App = () => {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    // get todo list from backend
-    const getTodos = async () => {
-      axios.get("http://localhost:8000/todos")
-        .then(res => {
-          setTodos(res.data)
-        })
-        .catch(err => {
-          console.log(err)
-        })
+    const fetchTodos = async () => {
+      try {
+        const todosData = await api.getTodos();
+        setTodos(todosData);
+      } catch (error) {
+        console.error("Failed to fetch todos:", error);
+      }
+    };
+    fetchTodos();
+  }, []);
+
+  const addTodo = async (text) => {
+    const newTodo = { id: Math.round(Math.random() * 1000), text: text };
+    try {
+      await api.addTodo(newTodo);
+      setTodos((prevTodos) => [...prevTodos, newTodo]);
+    } catch (error) {
+      console.error("Failed to add todo:", error);
     }
-    getTodos()
-  }, [])
+  };
 
-  const addTodo = (text) => {
-    const newTodo = {id: Math.round(Math.random() * 1000), text: text}
-    axios.post("http://localhost:8000/todos", newTodo)
-      .then(res => console.log(res))
-      .catch(err => console.log(err))
-    setTodos([...todos, newTodo])
-  }
-
-  const deleteTodo = (id) => {
-    axios.delete(`http://localhost:8000/todos/${id}`)
-    setTodos(todos.filter(todo => todo.id !== id))
-  }
+  const deleteTodo = async (id) => {
+    try {
+      await api.deleteTodo(id);
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+    } catch (error) {
+      console.error("Failed to delete todo:", error);
+    }
+  };
 
   return (
     <div className="text-center">
       <h1 className="text-4xl font-bold my-3">Todo List</h1>
-      <AddTodo addTodo={addTodo}/>
-      <Todos todos={todos} deleteTodo={deleteTodo}/>
+      <AddTodo addTodo={addTodo} />
+      <Todos todos={todos} deleteTodo={deleteTodo} />
     </div>
   );
-}
+};
 
 export default App;
+
